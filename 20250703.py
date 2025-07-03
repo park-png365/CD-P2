a3700628e977e689401f41eabcefef835777e2e3
park-png365 <parkjingue0921@gmail.com>
2025-07-03 20:52:55 +0900

    Add files via upload

---
+++ 20250703.py
@@ -0,0 +1,47 @@
+##a, b, c = map(int, input("입력 : " ).split())
+##if a >= b and a >= c :
+##    print(a)
+##elif b >= a and b >= c :
+##    print(b)
+##elif c >= a and c >= b :
+##    print(c)
+
+##n = int(input("입력 : "))
+##sum = 0
+##for i in range(1,n+1) :
+##    if i % 2 == 1 :
+##        sum = sum + i
+##        
+##print(sum)
+
+##n = int(input("입력 : "))
+##for i in range(1,n+1) :
+##    if i % 3 == 0 :
+##        i = "three"
+##    print(i)
+
+##a, b = map(int, input("입력 : " ).split())
+##for i in range(a,b+1) :
+##    print(i)
+        
+##n = int(input("입력 : "))
+##for i in range(n, 2, -1) :
+##    if i % 5 == 0 :
+##        print(i)
+
+##n = int(input("입력 : "))
+##if n%2 == 0 and n<10 :
+##    print("small even")
+##elif n%2 == 1 and n<10 :
+##    print("small odd")
+##elif  n>10 :
+##    print("large")
+
+
+        
+
+
+        
+        
+        
+              
