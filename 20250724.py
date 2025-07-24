8abab2925421f5cdd65e1651126959c59158691e
park-png365 <parkjingue0921@gmail.com>
2025-07-24 20:52:16 +0900

    Add files via upload

---
+++ 20250724.py
@@ -0,0 +1,67 @@
+1.##s = input("문자열 입력 : ")
+##print(len(s))
+
+2.##s = input("문자열 입력 : ")
+##if "@" in s :
+##    print( "이메일 형식입니다.")
+
+3.##s = input("문자열 입력 : ")
+##print(s.find("a"))
+
+4.##s = input("문자열 입력 : ")
+##print(s * 3)
+
+5.##s = input("문자열 입력 : ")
+##w = input("문자열 입력 : ")
+##print(s + w)
+
+6.##s = input("문자열 입력 : ")
+##print(s[::-1])
+
+7.##s = input("문자열 입력 : ")
+##lower_count = 0
+##for ch in s :
+##    if "a" <= ch <="z" :
+##        lower_count += 1
+##print(lower_count)
+
+8.##s = input("문자열 입력 : ")
+##print(s.count("a"))
+
+9.##s = input("문자열 입력 : ")
+##for ch in s :
+##    print(ch)
+
+10.##s = input("문자열 입력 : ")
+##if "A"<= s <= "Z" :
+##    print("대문자입니다.")
+
+1.##s = input("문자열 입력 : ")
+##print(s.swapcase())
+
+2.##s = input("문자열 입력 : ")
+##count = 0
+##for ch in s :
+##    if "0" <= ch  <= "9" :
+##        count += 1
+##print(count)
+
+3.##a = 0
+##s = input("문자열 입력 : ")
+##for ch in s:
+##    if not ("1" <= ch <= "9") :
+##        a = 1
+##
+##if a == 0 :
+##    print("숫자입니다.")
+
+4.##s = input("문자열 입력 : ")
+##if s[0] == s[-1] :
+##    print("같음")
+
+5.##s = input("문자열 입력 : ")
+##a = len(s)
+##print ("-" * a)
+    
+
+
