42e013074ff7d85bf95c480e6b73b492203f176a
park-png365 <parkjingue0921@gmail.com>
2025-08-07 20:47:13 +0900

    Add files via upload

--- 20250807.py
+++ 20250807.py
@@ -1,41 +1,124 @@
-6.##a = input("정수 : ")
-##for ch in a :
-##    if 'a' <= ch <= 'z' or 'A' <= ch <='Z' :
-##        print(ch)
+1.##nums = list(map(int,input().split()))
+##print(sum(nums))
 
-7.##a = input("정수 : ")
-##for ch in a :
-##    if '1' <= ch <= '9' :
-##        print(ch)
+2.##data = list(map(int,input().split()))
+##print(max(data))
 
-8.##a = input("정수 : ")
-##print(a.swapcase())
+3.##nums = list(map(int,input().split()))
+##for n in nums :
+##  if n % 2 ==0 :
+##    print(n)
 
-9.##a = input("정수 : ")
-##a = a[::-1]
-##for ch in a :
-##    print(ch)
+4.##nums = list(map(int,input().split()))
+##cnt = 0
+##for n in nu ms :
+##  if 5 <= n :
+##    cnt += 1
+##print(cnt)
 
-a = input("정수 : ")
-upper_count = 0
-lower_count = 0
-digit_count = 0
-etc_count = 0
-for ch in a :
-    if 'A' <= ch <= 'Z' :
-        upper_count += 1
-    elif 'a' <= ch <= 'z' :
-        lower_count += 1
-    elif '1' <= ch <= '9' :
-        digit_count += 1
-    else :
-        etc_count += 1
-print("대문자 개수 : " ,upper_count)
-print("소문자 개수 : " ,lower_count)
-print("숫자 개수 : " ,digit_count)
-print("기타문자 개수 : " ,etc_count)
+5.##nums = list(map(int,input().split()))
+##reversed_nums = nums[::-1]
+##print(*reversed_nums)
 
-    
+6.##nums = list(map(int,input().split()))
+##for n in nums :
+##  if n%2 == 1 :
+##    print(n)
+
+7.##nums = [10,15,8,20]
+##for i in range(len(nums)-1) :
+##  print(abs(nums[i] - nums[i+1]))
+
+8.##number = 0
+##nums = list(map(int,input().split()))
+##count = sum(nums)
+##num = len(nums)
+##number = count / num
+##for n in nums :
+##  if n > number :
+##    print(n)
+
+9.##nums = list(map(int,input().split()))
+##max = max(nums)
+##min = min(nums)
+##print(max)
+##print(min)
+
+10.##nums = list(map(int,input().split()))
+##arr = []
+##for n in nums :
+##  if n%2 == 0 :
+##    arr.append(n)
+##
+##print(arr[::-1])
+
+1.##nums = list(map(int,input().split()))
+##max_len = 1
+##cur_len = 1
+##for i in range(1,len(nums)) :
+##  if nums[i] - nums[i-1] > 0 :
+##    cur_len += 1
+##    max_len = max(max_len, cur_len)
+##  else :
+##    cur_len = 1
+##print(max_len);
+  
+2.##nums = list(map(int,input().split()))
+##max_val = max(nums)
+##min_val = min(nums)
+##print(max_val - min_val)
+
+3.##nums = list(map(int,input().split()))
+##max_diff = 0
+##for i in range(len(nums)-1) :
+##  diff = abs(nums[i] - nums[i+1])
+##  print(diff)
+##  if diff  > max_diff :
+##    max_diff  = diff
+##print(max_diff)
+
+4.##nums = list(map(int,input().split()))
+##print(nums.count(nums[-1]))
+
+5.##nums = list(map(int,input().split()))
+##num = 0
+##for n in nums :
+##  if 5 <= n :
+##    num += n
+##print(num)
+
+6.##nums = list(map(int,input().split()))
+##count = 0
+##for n in nums :
+##  if n%2 == 0 :
+##    print (n)
+##    count += 1
+##print(count)
+
+7.##nums = list(map(int,input().split()))
+##for n in nums :
+##  n = n*2
+##  print(n, end=" ")
+
+
+8.##number = 0
+##nums = list(map(int,input().split()))
+##count = sum(nums)
+##num = len(nums)
+##number = count / num
+##for n in nums :
+##  if n > number :
+##    print(n)
+
+
+9.##nums = list(map(int,input().split()))
+##for n in nums :
+##  if n > nums[0] :
+##    print(n)
+
+10.##nums = list(map(int,input().split()))
+##print(nums[::-1])
+
 
 
 
