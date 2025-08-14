1fbbba6f5f689519384523b1068348c5aafcef59
park-png365 <parkjingue0921@gmail.com>
2025-08-14 20:50:17 +0900

    Add files via upload

--- 20250814.py
+++ 20250814.py
@@ -1,50 +1,77 @@
-1.##a = int(input("정수 입력 : "))
-##b = int(input("정수 입력 : "))
-##print(a + b)
+1.##n = int(input("정수입력 : "))
+##m = int(input("정수입력 : "))
+##for i in range (n ,m+1 ,5) :
+##  print(i)
 
-2.##a = input("문자열 입력 : ")
-##b = input("문자열 입력 : ")
-##c = len(a)
-##d = len(b)
-##if c > d :
-##    print(a)
+2.##n = int(input())
+##nums = list(map(int, input().split()))
+##check = False
+##for i in range(n) :
+##  if (check): print(nums[i], end = ' ')
+##  if (nums[i]==10):check = True
+
+##n = int(input())
+##nums = list(map(int, input().split()))
+##index = -1
+##for i in range (n) :
+##  if (nums[i] == 10) : index = i
+##print(nums[i+1 : ])
+
+3.##n = int(input("정수입력 : "))
+##for i in range(n) :
+##  spaces = '^' * (n - i - 1)
+##  stars = '*' * (2 * i +1)
+##  print(spaces + stars + spaces)
+
+4.##n = int(input("정수입력 : "))
+##m = int(input("정수입력 : "))
+##if n<m :
+##  print(n)
 ##else :
-##    print(b)
+##  print(m)
 
-3.##n = int(input("정수 입력 : "))
-##for i in range( n, 0, -1) :
-##    print('*'*i)
+5.##---중요---
+##n = int(input())
+##arr = list(map(int, input().split()))
+##for i in range(0, n, 2) :
+##  print("%d" % [i], end =  " ")
 
-4.##a = input("문자열 입력 : ")
-##print('\"' + a + '\'')
+6.##s = input()
+##ans = 0
+##for i in range(len(s)) :
+##  if s[i].isalpha() :
+##    ans += 1
+##print(ans)
 
-5.##a = int(input("정수입력 : "))
-##if a>0 :
-##    print(a * 1)
-##else :
-##    print(a * -1)
+7.##s = input("문자열 입력 : ")
+##n = int(input("정수 입력 : "))
+##print(s + "+" + str(n))
 
-6.##a = int(input("정수입력 : "))
-##b = int(input("정수입력 : "))
-##if a == b :
-##    print(a + b)
-##else :
-##    print(a - b)
+8.##n = int(input("정수입력 : "))
+##m = int(input("정수입력 : "))
+##print(max(n, m))
 
-7.##n = int(input("정수 입력 : "))
-##i = 1
-##while n >= i :
-##    print (i)
-##    i += 1
+9.##s = input("문자열 입력 : ")
+##for i in range(len(s) -2) :
+##  print(s[i : i+3], end = ' ' )
 
-8.##n = int(input())
-##arr  = list(map(int,input().split()))
-##for i in range(len(arr)-1) :
-##    print(arr[i]-arr[i+1])
+10.##s = input("문자열 입력 : ")
+##print('\\' + s + '\\')
 
-9.##n = input("정수 입력 : ")
-##print(n[::-1])
+1.##N = int(input())
+##value = N
+##add  = 3
+##
+##while value <= 100 :
+##  print(value)
+##  value += add
+##  add += 3
 
-10.##a = input("정수 입력 : ")
-##print(a.count('1'))
+3.##n = list(input("문자열 입력 : "))
+##under_count = 0
+##for ch in n :
+##  if 'A' < ch <'Z' :
+##    under_count += 1
+##print(under_count) 
 
+  
