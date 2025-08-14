d37a6299f23cb856dd496f58e22ed4cddca5ed98
park-png365 <parkjingue0921@gmail.com>
2025-08-14 18:49:56 +0900

    Add files via upload

---
+++ 20250814.py
@@ -0,0 +1,50 @@
+1.##a = int(input("정수 입력 : "))
+##b = int(input("정수 입력 : "))
+##print(a + b)
+
+2.##a = input("문자열 입력 : ")
+##b = input("문자열 입력 : ")
+##c = len(a)
+##d = len(b)
+##if c > d :
+##    print(a)
+##else :
+##    print(b)
+
+3.##n = int(input("정수 입력 : "))
+##for i in range( n, 0, -1) :
+##    print('*'*i)
+
+4.##a = input("문자열 입력 : ")
+##print('\"' + a + '\'')
+
+5.##a = int(input("정수입력 : "))
+##if a>0 :
+##    print(a * 1)
+##else :
+##    print(a * -1)
+
+6.##a = int(input("정수입력 : "))
+##b = int(input("정수입력 : "))
+##if a == b :
+##    print(a + b)
+##else :
+##    print(a - b)
+
+7.##n = int(input("정수 입력 : "))
+##i = 1
+##while n >= i :
+##    print (i)
+##    i += 1
+
+8.##n = int(input())
+##arr  = list(map(int,input().split()))
+##for i in range(len(arr)-1) :
+##    print(arr[i]-arr[i+1])
+
+9.##n = input("정수 입력 : ")
+##print(n[::-1])
+
+10.##a = input("정수 입력 : ")
+##print(a.count('1'))
+
