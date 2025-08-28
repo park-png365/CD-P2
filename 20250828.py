69897772d57b937f288a268209dae02ac3f68fce
park-png365 <parkjingue0921@gmail.com>
2025-08-28 20:21:55 +0900

    Add files via upload

--- 20250828.py
+++ 20250828.py
@@ -1,37 +1,22 @@
-1.##A = int(input("정수 입력 : "))
-##N = int(input("정수 입력 : "))
-##K = int(input("정수 입력 : "))
+8.##N = int(input("정수 입력 : "))
 ##for i in range(0,N) :
-##    print(A)
-##    A = A-K
+##  print("a" * (N-i) + "z" * i, end = ""
 
-2.##N = input("정수 입력 : ")
-##for ch in N :
-##    print(ch)
+9.##arr = list(map(int, input("리스트 입력 : ").split()))
+##arr[2:7] = arr[2 : 7][::-1]
+##print(arr)
 
-4.##a = float(input("소수 입력 : "))
-##print(round(a, 2))
-
-5.##n = int(input("정수 입력 : "))
-##arr = list(map(int,input("리스트 입력 : ").split()))
-##ans = 0
-##for i in arr :
-##    if i != 0 :
-##        ans += 1
-##print("%d"%ans)
-
-6.##s = input("문자열 입력 : ")
-##cnt = 0
-##for i in range(len(s)) :
-##    if "a"<= s[i] and s[i]<="z" :
-##        print(s[i], end = " ")
-##        cnt += 1
-##if cnt == 0 :
-##    print(-1)
-        
-
-7.##N = int(input("정수 입력 : "))
-##arr = list(map(int,input("리스트 입력 : ").split()))
-
-
+A = int(input("정수 입력 : "))
+B = int(input("정수 입력 : "))
+C = int(input("정수 입력 : "))
+if A == B and A == C :
+  print(A)
+elif A == B:
+  print(C)
+elif B == C:
+  print(A)
+elif A == C:
+  print(B)
+elif A != B and B!= C :
+  print(max(A, B, C))
 
