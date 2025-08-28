e1d63c143c675dfd525b54f2a44b63a7fd34563f
park-png365 <parkjingue0921@gmail.com>
2025-08-28 18:55:00 +0900

    Add files via upload

---
+++ 20250828.py
@@ -0,0 +1,37 @@
+1.##A = int(input("정수 입력 : "))
+##N = int(input("정수 입력 : "))
+##K = int(input("정수 입력 : "))
+##for i in range(0,N) :
+##    print(A)
+##    A = A-K
+
+2.##N = input("정수 입력 : ")
+##for ch in N :
+##    print(ch)
+
+4.##a = float(input("소수 입력 : "))
+##print(round(a, 2))
+
+5.##n = int(input("정수 입력 : "))
+##arr = list(map(int,input("리스트 입력 : ").split()))
+##ans = 0
+##for i in arr :
+##    if i != 0 :
+##        ans += 1
+##print("%d"%ans)
+
+6.##s = input("문자열 입력 : ")
+##cnt = 0
+##for i in range(len(s)) :
+##    if "a"<= s[i] and s[i]<="z" :
+##        print(s[i], end = " ")
+##        cnt += 1
+##if cnt == 0 :
+##    print(-1)
+        
+
+7.##N = int(input("정수 입력 : "))
+##arr = list(map(int,input("리스트 입력 : ").split()))
+
+
+
